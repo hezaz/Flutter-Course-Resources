35ccd40795d573fd97746dba708389298a23ff25
hezaz <62071557+hezaz@users.noreply.github.com>
2022-09-06 03:43:56 +0200

    v++

--- micard/lib/main.dart
+++ micard/lib/main.dart
@@ -15,24 +15,89 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100,
-            width: 100,
-            margin: EdgeInsets.all(
-              10.0,
-            ),
-            padding: EdgeInsets.only(
-              left: 25,
-            ),
-            color: Colors.blue,
-            child: Text(
-              'Early Eyes',
-              style: TextStyle(
-                fontSize: 20,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 0,
+                width: double.infinity,
               ),
-            ),
+              CircleAvatar(
+                radius: 60,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('images/logo.png'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Early Eyes',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Young developers',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 140,
+                height: 1,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 100,
+                height: 10,
+              ),
+              Container(
+                width: 300,
+                height: 30,
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '+33 6 74 57 07 81',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 300,
+                height: 30,
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('EarlyEyes2022@gmail.com',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal,
+                        )),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
